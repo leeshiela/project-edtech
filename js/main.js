1b10497bfdd432d6a2db3d4e3a776098ce216342
Shiela <leeshiela@gmail.com>
2019-04-25 21:29:08 -0400

    sticky navbar

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//Sticky, disappearing horizontal navigation bar
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("horizontalNav").style.top = "0";
+  } else {
+    document.getElementById("horizontalNav").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
